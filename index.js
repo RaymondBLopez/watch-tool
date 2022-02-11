88f26dc60cce1c2be87e0f0dcd4ffc9df25a3853
Raymond Lopez <raymondblopez97@gmail.com>
2022-02-11 13:35:58 -0500

    add functionality to start program

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log('I was executed');
+const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+const program = require('caporal');
+
+const start = debounce(() => {
+  console.log('Starting Users program');
+}, 100);
+
+chokidar
+  .watch('.')
+  .on('add', start)
+  .on('change', () => console.log('File changed'))
+  .on('unlink', () => console.log('File unlinked'));
