4f3302cee43f90599576d76a6b02aa054df96cd4
Raymond Lopez <raymondblopez97@gmail.com>
2022-02-11 13:57:41 -0500

    add re-execution on change

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
 const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
 
-const start = debounce(() => {
-  console.log('Starting Users program');
-}, 100);
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
 
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('File changed'))
-  .on('unlink', () => console.log('File unlinked'));
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+      spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
